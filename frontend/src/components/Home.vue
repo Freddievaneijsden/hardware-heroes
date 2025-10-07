39d80c047b70a53d4086625a39ddc983d7c8ac51
Freddie Van Eijsden <freddievaneijsden@gmail.com>
2025-10-07 13:23:15 +0200

    Testing home component and clean up
    Co-authored-by: Maximillian Rubensson <maximoff87@gmail.com>
    Co-authored-by: Oskar <Oggeboii@users.noreply.github.com>
    Co-authored-by: JesperSandklef <JesperSandklef@users.noreply.github.com>

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref } from 'vue'
+import imageHome from '../assets/completeComputer-removebg-preview.png'
+
+const output = ref('Test')
+</script>
+
+<template>
+  <h1>Hello!</h1>
+  <p>{{ output }}</p>
+  <img :src="imageHome" alt="Computer" />
+</template>

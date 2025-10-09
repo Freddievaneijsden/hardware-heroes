c4f0d3ffd96e99213c03db5e7cd7833ebf8bf281
Oskar Dahlström <0skar01@hotmail.se>
2025-10-09 14:32:16 +0200

    Adds Navbar component
    Co-authored-by: Freddievaneijsden <Freddievaneijsden@users.noreply.github.com>

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,13 +3,19 @@ import { RouterLink, RouterView } from 'vue-router'
 import Home from './components/Home.vue'
 import UsersGet from './components/UsersGet.vue';
 import UsersCreate from './components/UsersCreate.vue';
+import Nav from './components/Nav.vue';
 
 </script>
 
 <template>
+  <header>
+  <Nav />
+  </header>
+  <body>
   <Home />
   <UsersCreate />
   <UsersGet />
+  </body>
 </template>
 
 <style scoped></style>
---
+++ frontend/src/components/Nav.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { ref } from 'vue'
+
+const output = ref('Test')
+</script>
+
+<template>
+  <header>
+    <h1>Hardware Heros</h1>
+    <div class="btnWrapper">
+      <button>Home</button>
+      <button>Login</button>
+    </div>
+  </header>
+</template>
+
+<style >
+
+h1 {
+  color:#000000;
+  text-align: center;
+}
+
+header {
+  display: flex;
+  justify-content: space-between; 
+  align-items: center;
+  background-color: #14B8A6;
+  padding: 10px 20px;
+  gap: 700px;
+}
+
+.btnWrapper {
+  display: flex;
+  gap: 10px;
+  height: 32px
+}
+button{
+    font-weight: bold;
+    border-radius: 20%;
+    background-color: #EC4899;
+}
+
+
+</style>

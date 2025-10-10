e0d189b82cdc8670ce18fa9ea10da959ee2c4248
Freddie Van Eijsden <freddievaneijsden@gmail.com>
2025-10-10 14:22:39 +0200

    Fetch user by name

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -2,19 +2,49 @@
 import { ref } from 'vue'
 
 const user = ref(null)
+const userName = ref(null)
 const success = ref(false)
 const error = ref(false)
 
-const validUsername = async () => {}
+const validUsername = async () => {
+  success.value = false
+  error.value = false
+
+  try {
+    const response = await fetch(
+      `http://localhost:3000/login/user?userName=${encodeURIComponent(userName.value)}`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+    )
+
+    const result = await response.json()
+    user.value = result.data
+
+    if (!response.ok) {
+      throw new Error('Could not find user with that username: ' + response.status)
+    }
+
+    success.value = true
+    userName.value = null
+  } catch (err) {
+    error.value = err.message
+  }
+}
 </script>
 
 <template>
   <div>
-    <form @submit.prevent="checkAccount">
-      <input type="text" placeholder="Username" required /> <br />
-      <input type="text" placeholder="Password" required /> <br />
+    <form @submit.prevent="validUsername">
+      <input v-model="userName" type="text" placeholder="Username" required /> <br />
+      <input type="password" placeholder="Password" required /> <br />
       <button>Login</button>
-      <p>{{ success }}</p>
     </form>
+    <p>{{ user }}</p>
+    <p v-if="success">✅ User found successfully!</p>
+    <p v-if="error">❌ {{ error }}</p>
   </div>
 </template>

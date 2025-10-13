713e69b44a204e7bf8bafe46196c9cf76e7857b4
Freddievaneijsden <166924808+Freddievaneijsden@users.noreply.github.com>
2025-10-13 10:39:38 +0200

    Login component (#3)
    * Create loginService and initial Login.vue
    
    * Create loginController
    
    * Create loginRoute
    
    * Update loginController with 404 if user not found and change to query param
    
    * Fetch user by name
    
    * Update loginController to compare password and POST

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,9 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 const userRoute = require('./routes/userRoute');
+const loginRoute = require('./routes/loginRoute');
 app.use(userRoute);
+app.use(loginRoute);
 
 app.listen(port, () => console.log(`Hardware Heroes listening on port ${port}!`));
 
---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { ref } from 'vue'
+
+const userName = ref(null)
+const userPassword = ref(null)
+const success = ref(false)
+const error = ref(false)
+
+const validLogin = async () => {
+  success.value = false
+  error.value = false
+
+  try {
+    const response = await fetch(`http://localhost:3000/login/user`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        userName: userName.value,
+        userPassword: userPassword.value,
+      }),
+    })
+
+    const result = await response.json()
+
+    if (!response.ok) throw new Error(result.error || 'Login failed')
+
+    console.log('✅ Logged in:', result)
+    success.value = true
+    userName.value = null
+    userPassword.value = null
+  } catch (err) {
+    error.value = err.message
+  }
+}
+</script>
+
+<template>
+  <div>
+    <form @submit.prevent="validLogin">
+      <input v-model="userName" type="text" placeholder="Username" required /> <br />
+      <input v-model="userPassword" type="password" placeholder="Password" required /> <br />
+      <button>Login</button>
+    </form>
+    <p v-if="success">✅ Successfull Login!</p>
+    <p v-if="error">❌ {{ error }}</p>
+  </div>
+</template>

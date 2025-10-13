951086f73dead054a9401b9e507462bdb97ab28e
Freddie Van Eijsden <freddievaneijsden@gmail.com>
2025-10-13 11:32:44 +0200

    Save token and user to localStorage

--- backend/routes/loginRoute.js
+++ backend/routes/loginRoute.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router(); 
 const loginController = require('../controller/loginController');
-const authenticateToken = require('../middleware/auth')
 
 router.post('/login/user', loginController.getUserByName);
 

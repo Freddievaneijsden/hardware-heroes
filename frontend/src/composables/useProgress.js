a72efcf427f673a0aa64d083acf592314b4e6df6
Freddie Van Eijsden <freddievaneijsden@gmail.com>
2025-10-27 13:23:36 +0100

    Add getCurrentChapter and updateProgressChapter

--- frontend/src/composables/useProgress.js
+++ frontend/src/composables/useProgress.js
@@ -1,3 +1,7 @@
+import { ref } from 'vue'
+
+const currentChapter = ref(null);
+
 const createNewProgress = async (userId) => {
   try {
     const response = await fetch('http://localhost:3000/users/progress', {
@@ -15,10 +19,54 @@ const createNewProgress = async (userId) => {
 
     if (!response.ok) throw new Error(result.error || 'Failed to create new progress')
   } catch (err) {
-    error.value = err.message
+     console.error(err.message);
   }
 }
 
+const updateProgressChapter = async (userId, currentChapter) => {
+    try {
+
+    const newChapter = currentChapter + 1
+    
+    const response = await fetch(`http://localhost:3000/users/progress/${userId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        progressChapterId: newChapter
+      }),
+    })
+
+    const result = await response.json()
+
+    if (!response.ok) throw new Error(result.error || 'Failed to update chapter')
+
+    } catch (err) {
+    console.error(err.message);
+  }
+}
+
+const getCurrentChapter = async (userId) => {
+    try {
+    const response = await fetch(`http://localhost:3000/users/progress/${userId}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    })
+
+    const result = await response.json()
+
+    if (!response.ok) throw new Error(result.error || 'Failed to get progress by userId')
+    
+    currentChapter.value = result.data[0].progressChapterId;
+
+    } catch (err) {
+    console.error(err.message);
+    }
+}
+
 export function useProgress() {
-  return { createNewProgress }
+  return { createNewProgress, updateProgressChapter, getCurrentChapter, currentChapter}
 }

586a870c7c8d7fe493a3c16624719f7d1f762f33
Oskar Dahlström <0skar01@hotmail.se>
2025-10-28 12:30:15 +0100

    refactor variable quizId to chapterId to better reflect method

--- backend/routes/quizRoute.js
+++ backend/routes/quizRoute.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const quizController = require('../controller/quizController');
 
 router.get('/quizzes', quizController.getquizzes);
-router.get('/quizzes/:quizId', quizController.getquizById);
+router.get('/quizzes/:chapterId', quizController.getquizByChapterId);
 
 module.exports = router;

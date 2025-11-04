be7d23d2335879850605190d746e7ac203d5a389
Freddie Van Eijsden <freddievaneijsden@gmail.com>
2025-11-04 09:17:38 +0100

    Remove Nav from HameView and add to App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,43 @@
 <script setup>
 import { RouterView } from 'vue-router'
-
+import Nav from '@/components/Nav.vue'
 </script>
 
 <template>
-  <main>    
-    <RouterView />
+  <main>
+    <Nav />
+    <RouterView v-slot="{ Component }">
+      <transition name="grow-in">
+        <Component :is="Component" />
+      </transition>
+    </RouterView>
   </main>
 </template>
 
 <style>
+.grow-in-enter-from {
+  opacity: 0;
+  transform: scale(0.3);
+}
+
+.grow-in-enter-to {
+  opacity: 1;
+}
+
+.grow-in-enter-active {
+  transition:
+    opacity 0.5s ease-out,
+    transform 0.5s ease-out;
+}
+
+.grow-in-leave-to {
+  opacity: 0;
+  transform: scale(0.3);
+}
+
+.grow-in-leave-active {
+  transition:
+    opacity 0.3s ease-in,
+    transform 0.3s ease-in;
+}
 </style>

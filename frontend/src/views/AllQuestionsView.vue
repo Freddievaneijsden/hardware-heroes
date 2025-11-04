e450178b75c31d38720a9e89438315c2048dea63
Oskar Dahlström <0skar01@hotmail.se>
2025-11-04 13:27:13 +0100

    questionDetails above questionList

--- frontend/src/views/AllQuestionsView.vue
+++ frontend/src/views/AllQuestionsView.vue
@@ -105,13 +105,14 @@ main {
 }
 
 @media (max-width: 600px) and (min-width: 375px) {
-  .grid {
+  .grid { 
     grid-template-columns: 2fr;
     gap: 0;
   }
   main {
     margin: 0;
     background-color: #fcd34d;
+    text-align: center;
   }
   body {
     padding: 0;
@@ -120,10 +121,14 @@ main {
 
   .question-list {
     padding: 20px 20px;
+    -ms-flex-order: 2;
+    order: 2;
   }
 
   .question-details {
     padding-top: 20px;
+    -ms-flex-order: 1;
+    order: 1;
   }
 
   .welcome-content {
